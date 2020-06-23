068b9f60900f86bdd3e16df614ee7c1acf781aff
AndreLss <andre_luis98@outlook.com>
2020-06-23 11:00:50 -0300

    Configura servicos iniciar para usuarios de teste

--- src/configs/seeds/servicos.js
+++ src/configs/seeds/servicos.js
@@ -1,12 +1,21 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('servico').del()
-    .then(function () {
+    .then(async function () {
       // Inserts seed entries
-      return knex('servico').insert([
+      await knex('servico').insert([
         { nome: 'Unha', icone_path: 'nail.svg' },
         { nome: 'Cabelo', icone_path: 'hair-cut.svg' },
         { nome: 'Maquiagem', icone_path: 'maquiagem.svg' }
+      ]);
+
+      return knex('profissional_servico').insert([
+        { profissional_id: 1, servico_id: 1, valor: 22, disponivel: true },
+        { profissional_id: 1, servico_id: 2, valor: 85, disponivel: true },
+        { profissional_id: 1, servico_id: 3, valor: 85, disponivel: false },
+        { profissional_id: 2, servico_id: 1, valor: 15, disponivel: true },
+        { profissional_id: 2, servico_id: 2, valor: 95, disponivel: true },
+        { profissional_id: 2, servico_id: 3, valor: 50, disponivel: true }
       ]);
     });
 };
